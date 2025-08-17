5a7e17bdf0158b4046f00d3b1c91cdbe2f1ec877
Julio Abraham Puente Guerrero <julio.puenteg@uanl.edu.mx>
2025-08-17 13:50:43 -0600

    Add files via upload

---
+++ AuditoriaBasica.psm1
@@ -0,0 +1,48 @@
+﻿function Obtener-UsuariosInactivos{
+    <#
+    .SYNOPSIS
+    Obtiene usuarios locales habilitados que nunca han inicidado sesión.
+    
+    .DESCRIPTION
+    Esta funcion busca cuentas locales habilitadas que no tienen fecha de último inicio de sesión.
+
+    .EXAMPLE
+    Obtener-UsuariosInactivos
+
+    .NOTES
+    Puede ayudar a detectar cuentas innecesarias o riesgosas en auditorías básicas.
+
+    #>
+    
+    Get-LocalUser | Where-Object { $_.Enabled -eq $true -and -not $_.LastLogon }
+}
+
+function Obtener-ServiciosExternos{
+    <#
+    .SYNOPSIS
+    Obtiene servicios en ejecucion que no pertenecen explícitamente a Windows.
+    
+    .DESCRIPTION
+    Filtra servicios activos cuyo nombre descriptivo no contiene el termino "Windows".
+
+    .EXAMPLE
+    Obtener-ServiciosExternos
+
+    .NOTES
+    Útil para detectar software de terceros corriendo en segundo plano.
+
+    #>
+    
+    Get-Service | Where-Object { $_.Status -eq "Running" -and $_.DisplayName -notmatch "Windows"}
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    #>
+
+}
